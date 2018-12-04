7b4eaa3f06c7fbcfbb2ce813a242d943255d403f
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-12-04 14:50:02 +0200

    WIP; fixed copy-paste error :D

--- src/main/java/com/unicenta/pos/api/AES256Cryptor.java
+++ src/main/java/com/unicenta/pos/api/AES256Cryptor.java
@@ -8,8 +8,8 @@ package com.unicenta.pos.api;
  * It is an OpenSSL compatible implementation
  *
  * JS example:
- * var encoded=CryptoJS.AES.encrypt(original,key).toString(CryptoJS.enc.Utf8);
- * var decoded=CryptoJS.AES.decrypt(encoded,key).toString(CryptoJS.enc.Utf8);
+ * var encoded=CryptoJS.AES.encrypt(original,key).toString(CryptoJS.enc.Utf8)
+ * var decoded=CryptoJS.AES.decrypt(encoded,key).toString(CryptoJS.enc.Utf8)
  */
 
 import java.util.Base64;
