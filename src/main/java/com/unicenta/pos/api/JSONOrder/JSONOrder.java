a5e17fe5ab1c8a6014f31a46be442f30ea60a019
Tudor Ilisoi <tudorilisoi@yahoo.com>
2021-03-13 10:26:47 +0200

    added operation to JSONOrder.java

--- src/main/java/com/unicenta/pos/api/JSONOrder/JSONOrder.java
+++ src/main/java/com/unicenta/pos/api/JSONOrder/JSONOrder.java
@@ -7,9 +7,22 @@ import java.util.*;
 import com.fasterxml.jackson.annotation.*;
 
 public class JSONOrder {
+    public static String OP_UPDATE="UPDATE";
+    public static String OP_PAY_AND_CLOSE="PAY_AND_CLOSE";
+
     private String placeID;
     private String userID;
+    private String operation;
     private List<JSONLine> lines;
+
+    @JsonProperty("operation")
+    public String getOperation() {
+        return operation;
+    }
+    @JsonProperty("operation")
+    public void setOperation(String value) {
+        this.operation = value;
+    }
 
     @JsonProperty("placeID")
     public String getPlaceID() {
